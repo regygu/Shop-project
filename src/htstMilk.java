c47f72af6268473b6a766c1f907efc3d1b7c5191
regygu <ugyan.gergely@gmail.com>
2017-02-18 17:13:35 +0100

    uhtMilk and htstMilk classes implemented

---
+++ src/htstMilk.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+/**
+ * Created by ugyan on 2017.02.18..
+ */
+
+// HTST - standing for High Temperature / Short Time pasteurization
+
+public class htstMilk extends Milk {
+
+    public htstMilk(long barCode, int volume, String manufacturer, Date expiration, double fatContent) {
+        super(barCode, volume, manufacturer, expiration, fatContent);
+    }
+}
---
+++ src/uhtMilk.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+/**
+ * Created by ugyan on 2017.02.18..
+ */
+
+// UHT - standing for Ultra High Temperature pasteurization
+
+public class uhtMilk extends Milk {
+
+    public uhtMilk(long barCode, int volume, String manufacturer, Date expiration, double fatContent) {
+        super(barCode, volume, manufacturer, expiration, fatContent);
+    }
+}
