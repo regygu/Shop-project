2f48597ccc44038361ca5136ce8a5477e026ab5a
regygu <ugyan.gergely@gmail.com>
2017-02-19 13:04:12 +0100

    MilkFactory class added

---
+++ src/MilkFactory.java
@@ -0,0 +1,19 @@
+import milk.*;
+
+import java.util.Date;
+
+/**
+ * Created by ugyan on 2017.02.19..
+ */
+public abstract class MilkFactory {
+
+    public Milk newUhtMilk(Long barCode, int volume, String manufacturer, Date expiration, double fatContent) {
+        Milk newuhtMilk = new uhtMilk(barCode, volume, manufacturer, expiration, fatContent);
+        return newuhtMilk;
+    }
+
+    public Milk newHtsttMilk(Long barCode, int volume, String manufacturer, Date expiration, double fatContent) {
+        Milk newhtstMilk = new htstMilk(barCode, volume, manufacturer, expiration, fatContent);
+        return newhtstMilk;
+    }
+}
