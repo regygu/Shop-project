fd3d474af368217915a344306f4b99507525b9f9
regygu <ugyan.gergely@gmail.com>
2017-02-19 12:47:53 +0100

    Food class added

---
+++ src/Food.java
@@ -0,0 +1,39 @@
+import java.util.Date;
+
+/**
+ * Created by ugyan on 2017.02.19..
+ */
+public class Food {
+    protected long barCode;
+    protected String manufacturer;
+    protected Date expiration;
+
+    public Food(long barCode, String manufacturer, Date expiration) {
+        this.barCode = barCode;
+        this.manufacturer = manufacturer;
+        this.expiration = expiration;
+    }
+
+    public long getBarCode() { return barCode; }
+
+    public boolean isItFresh() {
+        Date currentDate = new Date();
+        if (currentDate.after(this.expiration)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public String toString() { return "Barcode : " + this.getBarCode() + "\n" +
+            "Manufacturer : " + this.getManufacturer() + "\n" +
+            "Expiration : " + this.getExpiration() + "\n"; }
+
+}
