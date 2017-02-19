2386e5a5b92fa0bc005a12c2c4f52d6863d275b5
regygu <ugyan.gergely@gmail.com>
2017-02-19 16:40:35 +0100

    StoreFactory added, MilkFactory removed

---
+++ src/store/StoreFactory.java
@@ -0,0 +1,34 @@
+package store;
+
+import store.goods.Cheese;
+import store.goods.Milk;
+import store.goods.Soap;
+import store.goods.milk.htstMilk;
+import store.goods.milk.uhtMilk;
+import java.util.Date;
+
+/**
+ * Created by ugyan on 2017.02.19..
+ */
+public abstract class StoreFactory {
+
+    public Milk newUhtMilk(Long barCode, int volume, String manufacturer, Date expiration, double fatContent) {
+        Milk newuhtMilk = new uhtMilk(barCode, volume, manufacturer, expiration, fatContent);
+        return newuhtMilk;
+    }
+
+    public Milk newHtstMilk(Long barCode, int volume, String manufacturer, Date expiration, double fatContent) {
+        Milk newhtstMilk = new htstMilk(barCode, volume, manufacturer, expiration, fatContent);
+        return newhtstMilk;
+    }
+
+    public Cheese newCheese(double weight, double fatContent, long barCode, String manufacturer, Date expiration) {
+        Cheese newCheese = new Cheese(weight, fatContent, barCode, manufacturer, expiration);
+        return newCheese;
+    }
+
+    public Soap newSoap(long barCode, String manufacturer, char detergentValue) {
+        Soap newSoap = new Soap(barCode, manufacturer, detergentValue);
+        return newSoap;
+    }
+}
--- src/store/goods/Soap.java
+++ src/store/goods/Soap.java
@@ -6,4 +6,14 @@ import store.Goods;
  * Created by ugyan on 2017.02.19..
  */
 public class Soap extends Goods {
+    protected long barCode;
+    protected String manufacturer;
+    protected char detergentValue;
+
+    public Soap(long barCode, String manufacturer, char detergentValue) {
+        this.barCode = barCode;
+        this.manufacturer = manufacturer;
+        this.detergentValue = detergentValue;
+
+    }
 }
